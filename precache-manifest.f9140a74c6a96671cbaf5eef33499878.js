4fc2674b22199c1b812fca4ca16ec831635a9e80
fyupanquia <fyupanquia@outlook.com>
2021-04-26 20:17:54 -0500

    static

---
+++ precache-manifest.f9140a74c6a96671cbaf5eef33499878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db995abf267aaa021318a0c559aa0fde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35b4c186ee7ee2af5217",
+    "url": "/static/css/main.a700d4f9.chunk.css"
+  },
+  {
+    "revision": "ebf7d93aa9f9b7005236",
+    "url": "/static/js/2.59b775b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.59b775b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b4c186ee7ee2af5217",
+    "url": "/static/js/main.b2870b25.chunk.js"
+  },
+  {
+    "revision": "97ad9e083897a8286841",
+    "url": "/static/js/runtime-main.d7d8550f.js"
+  }
+]);
